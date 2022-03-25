012d7fadc1ad4583711c808d28fec24f570f25c7
smita-shinde203 <smitashinde203@gmail.com>
2022-03-25 09:47:56 +0530

    mui added

---
+++ src/Theme.js
@@ -0,0 +1,19 @@
+import { createTheme } from '@mui/material/styles';
+
+
+const theme = createTheme({
+  palette: {
+    background:{
+      default:'#040000'
+    },
+    primary:{
+      main:'#000000',
+      text:'#ffffff',
+    },
+    secondary:{
+      main:'#052FC2'
+    }
+  },
+});
+
+export default theme;
